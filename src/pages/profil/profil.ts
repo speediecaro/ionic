537aea6108cd588e7147f3f7bbb83ca92d4a73d6
CarolineCotePortable <speediecaro@gmail.com>
2018-10-02 04:16:39 -0400

    Test avec les pages

---
+++ src/pages/profil/profil.ts
@@ -0,0 +1,17 @@
+//import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+//import { PagePrincipalePage } from '../pagePrincipale/pagePrincipale';
+import { Component } from '@angular/core';
+import { NavController } from "ionic-angular";
+
+
+@Component({
+    selector: 'page-profil',
+    templateUrl: 'profil.html'
+  })
+export class ProfilPage {
+   
+    constructor(public navCtrl: NavController) {
+    
+    }
+}
+
